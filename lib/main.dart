26cdcae58faf0e771bcd2f315dafa0e2df0c54a7
worood <woroodmubarki@gmail.com>
2025-02-04 22:29:46 +0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:plants_app_ui/onboarding_screen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      title: 'Onboarding Screen',
+      home: OnboardingScreen(),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
