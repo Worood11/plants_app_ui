5090541e5cb3bf7f7f2bd30bbfb9a093aac086f2
worood <woroodmubarki@gmail.com>
2025-02-05 23:06:55 +0300

    Onboarding screen

--- lib/constants.dart
+++ lib/constants.dart
@@ -7,8 +7,7 @@ class Constants {
   static var descriptionOne =
       "Read how to care for plants in our rich plants guide.";
   static var titleTwo = "Find a plant lover friend";
-  static var descriptionTwo =
-      "Are you a plant lover? Connect with other plant lovers.";
+  static var descriptionTwo = "Connect with other plant lovers.";
   static var titleThree = "Plant a tree, green the Earth";
   static var descriptionThree =
       "Find almost all types of plants that you like here.";
--- lib/onboarding_screen.dart
+++ lib/onboarding_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:plants_app_ui/constants.dart';
+import 'package:plants_app_ui/login_page.dart';
 
 class OnboardingScreen extends StatefulWidget {
   const OnboardingScreen({super.key});
@@ -14,6 +15,19 @@ class _OnboardingScreenState extends State<OnboardingScreen> {
 
   @override
   Widget build(BuildContext context) {
+    List<Widget> _buildIndicator() {
+      List<Widget> indicators = [];
+
+      for (int i = 0; i < 3; i++) {
+        if (currentIndex == i) {
+          indicators.add(_indicator(true));
+        } else {
+          indicators.add(_indicator(false));
+        }
+      }
+      return indicators;
+    }
+
     return Scaffold(
       backgroundColor: Colors.white,
       appBar: AppBar(
@@ -23,7 +37,10 @@ class _OnboardingScreenState extends State<OnboardingScreen> {
           Padding(
               padding: const EdgeInsets.only(right: 20, top: 20),
               child: InkWell(
-                onTap: () => null, //login screen
+                onTap: () {
+                  Navigator.pushReplacement(context,
+                      MaterialPageRoute(builder: (_) => const Login()));
+                }, //login screen
                 child: const Text(
                   'Skip',
                   style: TextStyle(
@@ -46,49 +63,133 @@ class _OnboardingScreenState extends State<OnboardingScreen> {
             },
             controller: _pageController,
             children: [
-              Container(
-                padding: const EdgeInsets.only(left: 50, right: 50, bottom: 80),
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    SizedBox(
-                      height: 350,
-                      child: Image.asset('assets/images/plant-one.png'),
-                    ),
-                    const SizedBox(
-                      height: 20,
-                    ),
-                    Text(
-                      Constants.titleOne,
-                      textAlign: TextAlign.center,
-                      style: TextStyle(
-                        color: Constants.primaryColor,
-                        fontSize: 30,
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                    const SizedBox(
-                      height: 20,
-                    ),
-                    Text(
-                      Constants.descriptionOne,
-                      textAlign: TextAlign.center,
-                      style: const TextStyle(
-                        fontSize: 20,
-                        fontWeight: FontWeight.w400,
-                        color: Colors.grey,
-                      ),
-                    ),
-                    const SizedBox(
-                      height: 20,
-                    ),
-                  ],
-                ),
+              createPage(
+                image: 'assets/images/plant-one.png',
+                title: Constants.titleOne,
+                description: Constants.descriptionOne,
+              ),
+              createPage(
+                image: 'assets/images/plant-two.png',
+                title: Constants.titleTwo,
+                description: Constants.descriptionTwo,
+              ),
+              createPage(
+                image: 'assets/images/plant-three.png',
+                title: Constants.titleThree,
+                description: Constants.descriptionThree,
               ),
             ],
+          ),
+          Positioned(
+            bottom: 80,
+            left: 30,
+            child: Row(
+              children: _buildIndicator(),
+            ),
+          ),
+          Positioned(
+            bottom: 60,
+            right: 30,
+            child: Container(
+              padding: const EdgeInsets.all(4),
+              decoration: BoxDecoration(
+                shape: BoxShape.circle,
+                color: Constants.primaryColor,
+              ),
+              child: IconButton(
+                  onPressed: () {
+                    setState(() {
+                      if (currentIndex < 2) {
+                        currentIndex++;
+                        if (currentIndex < 3) {
+                          _pageController.nextPage(
+                              duration: const Duration(milliseconds: 300),
+                              curve: Curves.easeIn);
+                        }
+                      } else {
+                        Navigator.pushReplacement(context,
+                            MaterialPageRoute(builder: (_) => const Login()));
+                      }
+                    });
+                  },
+                  icon: const Icon(
+                    Icons.arrow_forward_ios,
+                    size: 24,
+                    color: Colors.white,
+                  )),
+            ),
           ),
         ],
       ),
     );
   }
+}
+
+class createPage extends StatelessWidget {
+  final String image;
+  final String title;
+  final String description;
+
+  const createPage({
+    super.key,
+    required this.image,
+    required this.title,
+    required this.description,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.only(left: 50, right: 50, bottom: 80),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          SizedBox(
+            height: 350,
+            child: Image.asset(image),
+          ),
+          const SizedBox(
+            height: 20,
+          ),
+          Text(
+            title,
+            textAlign: TextAlign.center,
+            style: TextStyle(
+              color: Constants.primaryColor,
+              fontSize: 30,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          const SizedBox(
+            height: 20,
+          ),
+          Text(
+            description,
+            textAlign: TextAlign.center,
+            style: const TextStyle(
+              fontSize: 20,
+              fontWeight: FontWeight.w400,
+              color: Colors.grey,
+            ),
+          ),
+          const SizedBox(
+            height: 20,
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+Widget _indicator(bool isActive) {
+  return AnimatedContainer(
+    duration: const Duration(milliseconds: 300),
+    height: 10.0,
+    width: isActive ? 20 : 0,
+    margin: const EdgeInsets.only(right: 5.0),
+    decoration: BoxDecoration(
+      color: Constants.primaryColor,
+      borderRadius: BorderRadius.circular(5),
+    ),
+  );
 }
